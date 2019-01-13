7f4a2d6bc54c5c770c13f8fd1c195a0874eb9251
iuoon <710731936@qq.com>
2019-01-13 22:07:10 +0800

    文件修改

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -4,111 +4,59 @@ Page({
     cateItems: [
       {
         cate_id: 1,
-        cate_name: "水果",
+        cate_name: "../../images/item1.png",
         ishaveChild: true,
         children:
           [
             {
               child_id: 1,
-              name: '樱桃',
+              name: '大众文学',
               image: "https://m.360buyimg.com/mobilecms/s357x357_jfs/t15613/296/2281640440/392160/6e019064/5a9a450dNa47bf95f.jpg!q50.jpg"
             },
             {
               child_id: 2,
-              name: '樱桃',
+              name: '类型文学',
               image: "https://m.360buyimg.com/mobilecms/s357x357_jfs/t15613/296/2281640440/392160/6e019064/5a9a450dNa47bf95f.jpg!q50.jpg"
             },
             {
               child_id: 3,
-              name: '樱桃',
+              name: '经典文学',
               image: "https://m.360buyimg.com/mobilecms/s357x357_jfs/t15613/296/2281640440/392160/6e019064/5a9a450dNa47bf95f.jpg!q50.jpg"
             },
             {
               child_id: 4,
-              name: '樱桃',
+              name: '网络文学',
+              image: "https://m.360buyimg.com/mobilecms/s357x357_jfs/t15613/296/2281640440/392160/6e019064/5a9a450dNa47bf95f.jpg!q50.jpg"
+            },
+            {
+              child_id: 5,
+              name: '绘本漫画',
+              image: "https://m.360buyimg.com/mobilecms/s357x357_jfs/t15613/296/2281640440/392160/6e019064/5a9a450dNa47bf95f.jpg!q50.jpg"
+            },
+            {
+              child_id: 6,
+              name: '影视改编',
               image: "https://m.360buyimg.com/mobilecms/s357x357_jfs/t15613/296/2281640440/392160/6e019064/5a9a450dNa47bf95f.jpg!q50.jpg"
             }
           ]
       },
       {
         cate_id: 2,
-        cate_name: "干果",
-        ishaveChild: true,
+        cate_name: "../../images/item2.png",
+        ishaveChild: false,
         children:
-          [
-            {
-              child_id: 1,
-              name: '夏威夷果',
-              image: "https://m.360buyimg.com/mobilecms/s357x357_jfs/t18901/57/1522219067/198105/1f4ad39/5acaccb2Nf4a6792b.jpg!q50.jpg"
-            },
-            {
-              child_id: 2,
-              name: '夏威夷果',
-              image: "https://m.360buyimg.com/mobilecms/s357x357_jfs/t18901/57/1522219067/198105/1f4ad39/5acaccb2Nf4a6792b.jpg!q50.jpg"
-            },
-            {
-              child_id: 3,
-              name: '夏威夷果',
-              image: "https://m.360buyimg.com/mobilecms/s357x357_jfs/t18901/57/1522219067/198105/1f4ad39/5acaccb2Nf4a6792b.jpg!q50.jpg"
-            },
-            {
-              child_id: 4,
-              name: '夏威夷果',
-              image: "https://m.360buyimg.com/mobilecms/s357x357_jfs/t18901/57/1522219067/198105/1f4ad39/5acaccb2Nf4a6792b.jpg!q50.jpg"
-            },
-            {
-              child_id: 5,
-              name: '夏威夷果',
-              image: "https://m.360buyimg.com/mobilecms/s357x357_jfs/t18901/57/1522219067/198105/1f4ad39/5acaccb2Nf4a6792b.jpg!q50.jpg"
-            },
-            {
-              child_id: 6,
-              name: '夏威夷果',
-              image: "https://m.360buyimg.com/mobilecms/s357x357_jfs/t18901/57/1522219067/198105/1f4ad39/5acaccb2Nf4a6792b.jpg!q50.jpg"
-            },
-            {
-              child_id: 7,
-              name: '夏威夷果',
-              image: "https://m.360buyimg.com/mobilecms/s357x357_jfs/t18901/57/1522219067/198105/1f4ad39/5acaccb2Nf4a6792b.jpg!q50.jpg"
-            },
-            {
-              child_id: 8,
-              name: '夏威夷果',
-              image: "https://m.360buyimg.com/mobilecms/s357x357_jfs/t18901/57/1522219067/198105/1f4ad39/5acaccb2Nf4a6792b.jpg!q50.jpg"
-            }
-          ]
+          []
       },
       {
         cate_id: 3,
-        cate_name: "蔬菜",
-        ishaveChild: true,
+        cate_name: "../../images/item3.png",
+        ishaveChild: false,
         children:
-          [
-            {
-              child_id: 1,
-              name: '有机上海青',
-              image: "https://m.360buyimg.com/mobilecms/s357x357_jfs/t2827/290/2563889921/292001/bf218791/576b843eN1f7e4b44.jpg!q50.jpg"
-            },
-            {
-              child_id: 2,
-              name: '有机上海青',
-              image: "https://m.360buyimg.com/mobilecms/s357x357_jfs/t2827/290/2563889921/292001/bf218791/576b843eN1f7e4b44.jpg!q50.jpg"
-            },
-            {
-              child_id: 3,
-              name: '有机上海青',
-              image: "https://m.360buyimg.com/mobilecms/s357x357_jfs/t2827/290/2563889921/292001/bf218791/576b843eN1f7e4b44.jpg!q50.jpg"
-            },
-            {
-              child_id: 4,
-              name: '有机上海青',
-              image: "https://m.360buyimg.com/mobilecms/s357x357_jfs/t2827/290/2563889921/292001/bf218791/576b843eN1f7e4b44.jpg!q50.jpg"
-            }
-          ]
+          []
       },
       {
         cate_id: 4,
-        cate_name: "海鲜",
+        cate_name: "../../images/item4.png",
         ishaveChild: false,
         children: []
       }
